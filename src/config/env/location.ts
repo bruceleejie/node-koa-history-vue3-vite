879395397b7575b49b481369962ad7b428c330f3
KINGANDWOOD\lijie5 <lijie5@cn.kwm.com>
2023-09-06 14:14:09 +0800

    去掉公司的mysql配置信息

--- src/config/env/location.ts
+++ src/config/env/location.ts
@@ -4,9 +4,5 @@ export const config = {
     // mainPath: "https://oss.cn.kwm.com/kwmpublic/kwm_desktop_file_test/static/web/index.html",
     mysql: {
         // 这里可以配置mysql的配置 host、user、password、database等
-        host: "172.17.12.114",
-        user: "root",
-        password: "3edcVFR$",
-        database: "kwm_md_center",
     }
 }
--- src/config/env/test.ts
+++ src/config/env/test.ts
@@ -2,9 +2,5 @@ export const config = {
     port: process.env.Port || 10006,
     mysql: {
         // 这里可以配置mysql的配置 host、user、password、database等
-        host: "172.17.12.114",
-        user: "root",
-        password: "3edcVFR$",
-        database: "kwm_md_center",
     }
 }
